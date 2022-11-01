b662e4b4f15387b2f17e6b1b0c9fcf56ff1397ec
gonzaf24 <gonzaf21@hotmail.com>
2022-11-01 14:20:57 +0100

    login label

--- src/pages/login.page.js
+++ src/pages/login.page.js
@@ -4,7 +4,7 @@ import Login from '../components/login.component'
 export default function LoginPage() {
    return (
       <section className="login-container-all">
-         <span className="h1-title-span pb-4 pt-4">Login</span>
+         <span className="h1-title-login pb-4 pt-4">Login</span>
          <Login />
       </section>
    )
