629e3a7afcb1d5a2c7fefca34a8b9c798105fed5
gonzaf24 <gonzaf21@hotmail.com>
2022-11-03 11:30:26 +0100

    aaa

---
+++ src/components/admin/product/index.js
@@ -0,0 +1,5 @@
+import EditProduct from './EditProduct';
+import NewProduct from './NewProduct';
+import ProductTable from './ProductTable';
+
+export default { EditProduct, NewProduct, ProductTable };
