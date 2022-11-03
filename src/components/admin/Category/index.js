10bd06de5fc7c5025e31544c25d316d0c18230cb
gonzaf24 <gonzaf21@hotmail.com>
2022-11-03 11:23:00 +0100

    mmmm

--- src/components/admin/Category/index.js
+++
@@ -1,9 +0,0 @@
-import CategoryTable from './CategoryTable';
-import EditCategory from './EditCategory';
-import NewCategory from './NewCategory';
-
-export {
-  CategoryTable,
-  EditCategory,
-  NewCategory,
-};
--- src/components/admin/product/index.js
+++
@@ -1,9 +0,0 @@
-import EditProduct from './EditProduct';
-import ProductTable from './ProductTable';
-import NewProduct from './NewProduct';
-
-export {
-  EditProduct,
-  ProductTable,
-  NewProduct,
-}
