2954f94e78d91a5f21783cfb4142e8c5b6811613
gonzaf24 <gonzaf21@hotmail.com>
2022-11-05 22:02:49 +0100

    effects

--- src/components/SliderCarousel/SliderCarousel.jsx
+++ src/components/SliderCarousel/SliderCarousel.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick'
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io'; 
 
 import './SliderCarousel.css';
 
@@ -19,6 +20,21 @@ const defaultProps = {
   id: undefined,
 };
 
+// eslint-disable-next-line react/prop-types
+function SampleNextArrow({ onClick }) {
+  return (
+    <IoIosArrowForward className='SliderCarouselNextArrow' onClick={ onClick } />
+
+  );
+}
+
+// eslint-disable-next-line react/prop-types
+function SamplePrevArrow({ onClick }) {
+  return (
+    <IoIosArrowBack className='SliderCarouselPrevArrow' onClick={ onClick } />
+  );
+}
+
 
 const settings = {
   dots: false,
@@ -27,8 +43,9 @@ const settings = {
   slidesToShow: 1,
   slidesToScroll: 1,
   swipeToSlide: true,
+  nextArrow: <SampleNextArrow />,
+  prevArrow: <SamplePrevArrow />
 }
-
 
 const SliderCarousel = ({ className, id, children }) => {
   const classComponent = ['SliderCarousel', className].join(' ').trim();
